fdb589a1f77d342f39a07891060c50726ba1e0a0
EQualityn <egor120202@gmail.com>
2022-12-16 16:51:59 +0300

    experiment commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.printf("Testing Git");
+        System.out.println("Testing Git");
     }
 }
