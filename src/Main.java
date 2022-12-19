55ee56ec5d1a1c56cc949376d5e5238ac40a2d17
EQualityn <egor120202@gmail.com>
2022-12-19 20:04:55 +0300

    Hayam poem

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Testing Git");
-        Person person = new Person();
-        person.setName("George");
-        System.out.println(person.getName());
-
+        System.out.println("\t\tРУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("\t\t\t\tО. Хайям\n");
     }
 }
