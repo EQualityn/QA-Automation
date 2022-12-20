af6567fd43702744564bfdbd841ab6beb89b95e3
EQualityn <egor120202@gmail.com>
2022-12-20 18:46:27 +0300

    feat: howework complete

---
+++ src/lesson_2/HomeTask/HomeTask_1.java
@@ -0,0 +1,39 @@
+package lesson_2.HomeTask;
+
+public class HomeTask_1 {
+    public static void main(String[] args) {
+    // task 1
+    byte byte_1 = 9;
+    byte byte_2 = -9;
+    short short_1 = 19;
+    short short_2 = -19;
+    int int_1 = 199;
+    int int_2 = -199;
+    long long_1 = 1999L;
+    long long_2 = -1999L;
+    System.out.println(byte_1+"\t"+byte_2+"\n"
+            +short_1+"\t"+short_2+"\n"
+            +int_1+"\t"+int_2+"\n"
+            +long_1+"\t"+long_2+"\n");
+    //task 2
+        boolean bool_1=true;
+        boolean bool_2=false;
+        float float_1=1.1F;
+        float float_2=1.0F;
+        double double_1=3.5;
+        double double_2=1.0;
+        System.out.println(bool_1+"\t"+bool_2+"\n"
+                +float_1+"\t"+float_2+"\n"
+                +double_1+"\t"+double_2+"\n");
+        //task 3
+        char char_1='a';
+        char char_2='\u0758';
+        char char_3 = 0;
+        char char_4 = 0x6A;
+        System.out.println(char_1+"\n"+char_2+"\n"+
+                char_3+"\n"+char_4+"\n");
+
+
+
+    }
+}
