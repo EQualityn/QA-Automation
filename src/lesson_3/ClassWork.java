748cf1794ff6b63581a3895cdbd234ca474e688b
EQualityn <egor120202@gmail.com>
2022-12-22 23:01:11 +0300

    working with scanner

---
+++ src/lesson_3/ClassWork.java
@@ -0,0 +1,14 @@
+package lesson_3;
+
+import java.util.Scanner;
+
+public class ClassWork {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println(input.next());
+        System.out.println(input.nextLine());
+
+    }
+}
