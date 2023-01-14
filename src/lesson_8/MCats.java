984eadc7cd2f3d58637c66de029cd362586c3c1d
EQualityn <egor120202@gmail.com>
2023-01-14 15:56:53 +0200

    feat: add Cat keyboard input

--- src/lesson_8/MCats.java
+++ src/lesson_8/MCats.java
@@ -1,15 +1,19 @@
 package lesson_8;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MCats {
     public static void main(String[] args) {
         ArrayList<Cat> cats = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
             cats.add(new Cat("Atos", "Black", 8));
             cats.add(new Cat("Portos", "Gray", 9));
             cats.add(new Cat("Aramis","White", 7));
             cats.add(new Cat("D'artanjan", "Blue", 6));
-            cats.add((new Cat("Monsieur Rischelleux", "Red", 15)));
+            cats.add(new Cat("Monsieur Rischelleux", "Red", 15));
+            System.out.println("Enter Cat's name, color, age. Each parameter on a new line");
+            cats.add(new Cat(input.next(), input.next(), input.nextInt()));
             cats.forEach((Cat cat) -> System.out.println(cat.toString()));
     }
 }
